5b7d573eaee1d38b61f6ac8a3f57ddfc73f9d568
apollinaria-z <polina.zolotaya@gmail.com>
2020-11-15 17:54:23 +0300

    second react-app study

---
+++ my-second-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+    <header className='header'>
+            <img src='https://www.creativefabrica.com/wp-content/uploads/2018/07/Crazy-Frog-Logo-by-Pedro-Alexandre-Teixeira-312x208.png'/>
+        </header>
+    );
+}
+
+export default Header;
